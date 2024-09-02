be62c1444d96d0b4a1680ae42c2ade3e4f5a719f
Jordan <jordan.l.partridge@gmail.com>
2024-09-01 22:56:06 -0700

    Create user and token from command
    simplify fqn
    
    Dusting
    
    Ignore Dusting commit in git blame

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,48 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Validation\Rules\Password;
+
+use function Laravel\Prompts\confirm;
+use function Laravel\Prompts\password;
+use function Laravel\Prompts\text;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('make:user', function () {
+    $name = text(
+        label: 'What is the name of the user?',
+        placeholder: 'Jordan Partridge', validate: ['required', 'max:255', 'min:2']
+    );
+
+    $email = text(
+        label: 'What is the email of the user?',
+        placeholder: 'jordan@partridge.rocks', validate: ['required', 'email', 'unique:users,email']
+    );
+
+    $password = password(
+        label: 'What is the password of the user?',
+        placeholder: 'SuperSecret', validate: ['required', Password::defaults()]
+    );
+
+    $apiToken = confirm('Would you like to generate an API token for the user?',
+        default: true);
+
+    $user = User::create([
+        'name' => $name,
+        'email' => $email,
+        'password' => $password,
+    ]);
+
+    if ($apiToken) {
+        $token = $user->createToken('api_token')->plainTextToken;
+        $this->info($token);
+    }
+
+    $this->info('User created successfully');
+
+});
