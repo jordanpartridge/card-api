4a172b2b378e00669c39685367b1bdbcbbcbc1ed
Jordan <jordan.l.partridge@gmail.com>
2024-09-07 18:06:54 -0700

    unauthenticated in json even when accept json header not present

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -23,5 +27,27 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+        $exceptions->render(function (\Throwable $e, Request $request) {
+            Log::error('Exception caught:', [
+                'exception' => get_class($e),
+                'message' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+
+            if ($e instanceof AuthenticationException || $e->getMessage() === 'Route [login] not defined.') {
+                return response()->json(['message' => 'Unauthenticated.'], 401);
+            }
+
+            if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+                return response()->json(['message' => 'Not Found.'], 404);
+            }
+
+            if ($e instanceof HttpException) {
+                return response()->json(['message' => $e->getMessage()], $e->getStatusCode());
+            }
+
+            // Catch-all for any other exceptions
+            return response()->json(['message' => 'An error occurred.'], 500);
+        });
+    })
+    ->create();
