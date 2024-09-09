c8bdeb1e06ca393cc02772cd2f1c3bdd320e3e6a
Jordan <jordan.l.partridge@gmail.com>
2024-09-08 19:48:51 -0700

    support custom pagination

--- app/Http/Controllers/Cards/IndexController.php
+++ app/Http/Controllers/Cards/IndexController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\Cards;
 
 use App\Http\Resources\CardResource;
 use App\Models\Card;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 final class IndexController
 {
-    public function __invoke()
+    public function __invoke(Request $request): AnonymousResourceCollection
     {
-        return CardResource::collection(Card::query()->paginate());
+        return CardResource::collection(Card::query()->paginate($request->query('paginate', 15)));
     }
 }
