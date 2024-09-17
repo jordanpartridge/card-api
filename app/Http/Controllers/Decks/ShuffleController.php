ed23969e7d8c8da45ece668aff7330f0370e0c25
Jordan Partridge <jordan.l.partridge@gmail.com>
2024-09-16 17:53:24 -0700

    include reset functionality (#16)

--- app/Http/Controllers/Decks/ShuffleController.php
+++ app/Http/Controllers/Decks/ShuffleController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Decks;
 
 use App\Http\Resources\CardResource;
 use App\Models\Deck;
+use Illuminate\Http\Request;
 
 final class ShuffleController
 {
-    public function __invoke(Deck $deck)
+    public function __invoke(Request $request, Deck $deck)
     {
+        if ($request->input('reset')) {
+            $deck->initialize();
+        }
+
         $deck->shuffle();
 
         return CardResource::collection($deck->cards);
